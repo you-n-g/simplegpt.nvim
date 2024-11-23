df3ac3eb77d0642a077496435da685d9b7cf82e4
Young <afe.young@gmail.com>
2024-11-23 14:50:50 +0000

    feat: refactor shortcuts

--- lua/simplegpt/mappings.lua
+++ lua/simplegpt/mappings.lua
@@ -9,60 +9,60 @@ local shortcut = require("simplegpt.shortcut")
 function M.setup_main_mappings()
   vim.keymap.set(
     "n",
-    "<LocalLeader>gl",
+    "<m-g><m-g>l",
     loader.tele_load_reg,
     { noremap = true, silent = true, desc = "load registers" }
   )
   vim.keymap.set(
     "n",
-    "<LocalLeader>gD",
+    "<m-g><m-g>D",
     loader.input_dump_name,
     { noremap = true, silent = true, desc = "dump registers" }
   )
-  vim.keymap.set({ "n", "v" }, "<LocalLeader>ge", function()
+  vim.keymap.set({ "n", "v" }, "<m-g><m-g>e", function()
     local rqa = tpl.RegQAUI()
     rqa:build()
   end, { noremap = true, silent = true, desc = "edit registers" })
   vim.keymap.set(
     { "n", "v" },
-    "<LocalLeader>gs",
+    "<m-g><m-g>s",
     shortcut.build_func("clipboard"),
     { noremap = true, silent = true, desc = "send question2clipboard" }
   )
   vim.keymap.set(
     { "n", "v" },
-    "<LocalLeader>gc",
+    "<m-g><m-g>c",
     shortcut.build_func("chat"),
     { noremap = true, silent = true, desc = "send question2ChatGPT" }
   )
   vim.keymap.set(
     { "n", "v" },
-    "<LocalLeader>gr",
+    "<m-g><m-g>r",
     shortcut.build_func("popup"),
     { noremap = true, silent = true, desc = "send to get direct response" }
   )
   vim.keymap.set(
     { "n", "v" },
-    "<LocalLeader>gd",
+    "<m-g><m-g>d",
     shortcut.build_func("diff"),
     { noremap = true, silent = true, desc = "send to get response with diff" }
   )
   -- utils
   vim.keymap.set(
     { "n", "v" },
-    "<LocalLeader>gR",
+    "<m-g><m-g>R",
     target.resume_last_dialog,
     { noremap = true, silent = true, desc = "resume last dialog" }
   )
   vim.keymap.set(
     { "n", "v" },
-    "<LocalLeader>gp",
+    "<m-g><m-g>p",
     tpl.repo_load_file,
     { noremap = true, silent = true, desc = "load current file to reg" }
   )
   vim.keymap.set(
     { "n", "v" },
-    "<LocalLeader>gP",
+    "<m-g><m-g>P",
     tpl.repo_append_file,
     { noremap = true, silent = true, desc = "append current file to reg" }
   )
@@ -75,8 +75,8 @@ function M.setup()
   local ok, which_key = pcall(require, "which-key")
   if ok then
     which_key.add({
-      { "<LocalLeader>g", group = "SimpleGPT" },
-      { "<LocalLeader>s", group = "Shortcuts" },
+      { "<m-g><m-g>", group = "Advanced" },
+      { "<m-g>", group = "🤏SimpleGPT" },
     })
   end
 end
